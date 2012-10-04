bb1032e07d2d47c01d2318bd376c94574c5d3cf1
Dražen Lučanin <kermit666@gmail.com>
2012-10-04 09:54:06 +0200

    getting instance status

--- openstack/console_api.py
+++ openstack/console_api.py
@@ -10,8 +10,23 @@ import subprocess
 def list_files():
     resp = subprocess.call("ls -hal", shell=True)
     print(resp)
+
+def execute(command):
+    resp = subprocess.call(command, shell=True)
+    return resp
+
+def authenticate():
+    command = ". ~/creds/openrc"
+    if execute(command)==0: print("success!")
+
+def instance_info(instance_name):
+    command = "nova show " + instance_name
+    print(execute(command))
     
 def basic_test():
-    list_files()
+    #list_files()
+    authenticate()
+    instance_info("test-image")
+
 if __name__ == '__main__':
     basic_test()
