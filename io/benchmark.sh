cf98808ab5af460d319e2bca79cfbc45b0c315b1
Dražen Lučanin <kermit666@gmail.com>
2012-10-18 15:58:09 +0200

    small fixes in the benchmark script

--- io/benchmark.sh
+++ io/benchmark.sh
@@ -2,4 +2,4 @@
 #ssh 192.168.100.4 
 # ServerAliveInterval - koliko cesto slati upite je li server ziv
 # ServerAliveCountMax - koliko neodgovorenih upita tolerirati
-ssh -o ServerAliveInterval 60 ServerAliveCountMax 7200 192.168.100.4 python projekti/lp/berserk/trunk/berserk.py
+ssh -o "ServerAliveInterval=60" -o "ServerAliveCountMax=7200" 192.168.100.4 python projekti/lp/berserk/trunk/berserk.py
--- philharmonic/scheduler/conf.py
+++ philharmonic/scheduler/conf.py
@@ -7,8 +7,8 @@ Created on Oct 9, 2012
 # I/O
 #======
 
-#historical_en_prices_file = "./io/energy_price_data-quick_test.csv"
-historical_en_prices = "./io/energy_price_data-test.csv"
+historical_en_prices = "./io/energy_price_data-quick_test.csv"
+#historical_en_prices = "./io/energy_price_data-test.csv"
 
 results = "./io/results.pickle"
 
@@ -16,7 +16,7 @@ results = "./io/results.pickle"
 #===========
 
 # if dummy == True, will do just a local dummy benchmark, faking all the OpenStack commands
-dummy = True
+dummy = False
 # for False set all these other settings...
 
 # host on which the benchmark VM is deployed (for energy measurements)
@@ -30,8 +30,8 @@ instance = "kermit-test"
 command = "./io/benchmark.sh"
 
 # how many % of hours in a day should the VM be paused
-#percentage_to_pause = 0.04 # *100%
-percentage_to_pause = 0.15 # *100%
+percentage_to_pause = 0.04 # *100%
+#percentage_to_pause = 0.15 # *100%
 
 # time to sleep between checking if the benchmark finished or needs to be paused
 sleep_interval = 1 # seconds
