fd604ed1451a95724c1e78f650639000845fd66e
Dražen Lučanin <kermit666@gmail.com>
2012-10-29 14:10:13 +0100

    set time to start the experiment

--- schedule.py
+++ schedule.py
@@ -4,7 +4,19 @@ Created on Oct 15, 2012
 @author: kermit
 '''
 from philharmonic.scheduler.peak_pauser import PeakPauser
+import sys
+from datetime import datetime
+import time
 
 if __name__ == "__main__":
     scheduler = PeakPauser()
+    # waiting for the start time
+    if len(sys.argv)>1:
+        when = datetime.strptime(sys.argv[1], "%y-%m-%d %H:%M")
+        #to_start = datetime.combine(datetime.now().date(), when.time())
+        to_start = when
+        print("will start at %s." % to_start)
+        while datetime.now()<to_start:
+            time.sleep(1)
+    # start
     scheduler.run()
