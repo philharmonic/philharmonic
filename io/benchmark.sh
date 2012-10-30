0e052835b5d0073c68da9b2be47a8aa255666703
Dražen Lučanin <kermit666@gmail.com>
2012-10-30 17:27:23 +0100

    async ssh to start benchmark

--- io/benchmark.sh
+++ io/benchmark.sh
@@ -3,4 +3,6 @@
 # ServerAliveInterval - koliko cesto slati upite je li server ziv
 # ServerAliveCountMax - koliko neodgovorenih upita tolerirati
 #ssh -o "ServerAliveInterval=60" -o "ServerAliveCountMax=7200" 192.168.100.4 python projekti/lp/berserk/trunk/berserk.py
-ssh 192.168.100.4 "cd projekti/lp/berserk/trunk/; python berserk.py"
+#ssh 192.168.100.4 "cd projekti/lp/berserk/trunk/; python berserk.py"
+
+ssh kermit@192.168.100.4 "cd ~/projekti/lp/berserk/trunk/; nohup python berserk.py > berserk.out 2>berserk.err < /dev/null &"
