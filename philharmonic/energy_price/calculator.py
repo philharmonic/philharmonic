0e969b36075ca65fd0c5b192d59ca8aadf0d27d3
Dražen Lučanin <kermit666@gmail.com>
2013-04-12 16:30:48 +0200

    clean the calculator code

--- philharmonic/energy_price/calculator.py
+++ philharmonic/energy_price/calculator.py
@@ -42,41 +42,16 @@ def calculate_price_old(power, price_file, start_date=None, old_parser=False):
     total_price = h * sum(power*experiment_prices)
     return total_price
 
-def _calculate_price(power, price, compare_with=None):
-    """calculate energy consumption price based on an hourly series
-    of prices ($/kWh) and an houlry series of power values (W)
-    
-    @return: calculated price in $
-    
-    """
-    #TODO: this function assumes hourly mean power aggregation and
-    #the ending price at 00:00
-
-    # # optional plotting
-    # t = pd.offsets.Day(3)
-    # start = P_here.index[0]
-    # power[:start+t].plot(color=s1)
-    # figure()
-    # price[:start + t].plot(color=s2)
-    # if not compare_with is None:
-    #     figure()
-    #     compare_with[:start + t].plot(color=s3)
-    
-    price = joul2kwh(price[-1])
-    #print(len(power))
-    #print(len(price))
-    #side_by_side(power, price)
-    hourly_cost = power * price
-    cost = hourly_cost.sum()
-    return cost
-
-def calculate_price(power, prices=None, start_date=None, old_parser=False):
+def calculate_price(power, prices, start_date=None):
     """take or parse from a file a series of electricity prices ($/kWh),
     realign it to start_date (if it's provided) and calculate the price of the
     energy consumption stored in a time series of power values power (W)
     
-    @param power: pandas.Series of price values over time or a path to the
-    file to be parsed. 
+    @param power: pandas.TimeSeries of power values
+    @param prices: pandas.TimeSeries of price values or a path to a
+    file to parse it from.
+    @param start_date: Timestamp to which to realign the prices
+    (e.g. the starting index value for the power series)
     
     @return: calculated price in $
     
@@ -88,42 +63,39 @@ def calculate_price(power, prices=None, start_date=None, old_parser=False):
         prices = realign(prices, start_date)
     prices = per_kwh2per_joul(prices) # convert into $/J
     
+    #TODO: replace this with resample() and pass integrate as a function
+    # power.resample(prices.index.freq, how=calculate_energy, closed='both')*prices
+    #---
     times = list(power.index)
-    our_prices_raw = [] # here we'll store prices during the experiment
+    prices_list = [] # here we'll store prices during the experiment
     for t in times: # TODO: add a changing h value (per hour) and charge per hour
-        our_prices_raw.append(prices.asof(t))
-    experiment_prices = pd.Series(our_prices_raw, index = power.index)
+        prices_list.append(prices.asof(t))
+    experiment_prices = pd.Series(prices_list, index = power.index)
     
+    # we don't really know when the last interval ended, so we'll make a guess
     times = power.index
     N = float(len(times) + 1)
     t_0 = times[0]
-    # we don't really know when the last interval ended, so we'll make a guess
     t_N = times[-1]+(times[-1]-times[-2])
+    
     duration = (t_N - t_0)
     h = duration.total_seconds()/N
     
-    # TODO: use a library for this e.g. 
-    #   http://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html
     total_price = h * sum(power*experiment_prices)
+    #---
     return total_price
 
-def calculate_energy(power):
+def calculate_energy(power, estimate=False):
     """Calculates an integral of power over time
     using the rectangle method.
     @param power: pandas.Series of power samples (in Watts) 
     @return: calculated energy in Joules
     
     """
-    # manual rectangle rule
-#    times = power.index
-#    N = float(len(times) + 1)
-#    t_0 = times[0]
-#    # we don't really know when the last interval ended, so we'll make a guess
-#    t_N = times[-1]+(times[-1]-times[-2])
-#    duration = (t_N - t_0)
-#    h = duration.total_seconds()/N
-#    en = h*sum(power)
+    if estimate: # we won't integrate it
+        return power.mean() * len(power.resample('s'))
     # we don't really know when the last interval ended (and at what value), so we'll make a guess
+    # TODO: replace this using resample(closed='both')
     new_tick = power.index[-1]+(power.index[-1]-power.index[-2])
     power = power.append(pd.Series({new_tick: power[-1]}))
     # calculate the integral
