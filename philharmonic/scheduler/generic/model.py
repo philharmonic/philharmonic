16cb4f48a34bf2ac5744d0b78f0e58d9d22708f3
Dražen Lučanin <kermit666@gmail.com>
2013-07-11 17:50:51 +0200

    first version of State

--- philharmonic/scheduler/generic/model.py
+++ philharmonic/scheduler/generic/model.py
@@ -3,6 +3,7 @@ Created on Jul 11, 2013
 
 @author: kermit
 '''
+import copy
 
 # some non-semantic functionality common for VMs and servers
 class Machine(object):
@@ -20,6 +21,7 @@ class Machine(object):
 
 # the model
 class VM(Machine):
+    
     def __init__(self, *args):
         super(VM, self).__init__(*args)
         self.res = self.spec
@@ -29,6 +31,14 @@ class Server(Machine):
         super(Server, self).__init__(*args)
         self.cap = self.spec
         self.alloc = set()
+        
+    def migrate(self, vm, s):
+        try:
+            self.alloc.remove(vm)
+            s.alloc.add(vm)
+        except ValueError:
+            print('VM not here')
+            raise
 
 # constraint checking
 # C1
@@ -60,3 +70,47 @@ def all_within_capacity(servers):
         if not within_capacity(s):
             return False
     return True
+
+# Schedule
+# ==========
+
+class State():
+    """the state of the cloud at a single moment"""
+    def __init__(self, servers=None, VMs=None):
+        self.servers = servers
+        self.VMs = VMs
+        
+    def __deepcopy__(self, memo):
+        new_state = State()
+        new_state.__dict__.update(self.__dict__)
+        new_state.servers = []
+        new_state.VMs = []
+        for s in self.servers:
+            new_server = copy.copy(s)
+            # we keep the original vm objects!
+            new_server.alloc = set()
+            for vm in s.alloc:
+                new_state.VMs.append(vm)
+                new_server.alloc.add(vm)
+            new_state.servers.append(new_server)
+        return new_state
+             
+    
+    def __repr__(self):
+        return '%s , %s' % (self.servers.__repr__(), self.VMs.__repr__())
+        
+    def transition(self, migration):
+        """transition acccording to migration"""
+        new_state = copy.deepcopy(self)
+        new_state
+
+        
+class Migration():
+    """ migrate vm to server """
+    def __init__(self, vm, server):
+        self.vm = vm
+        self.server = server
+        
+class Schedule():
+    """initial state and a time series of migrations"""
+    pass
--- philharmonic/tests/generic_scheduler/model_test.py
+++ philharmonic/tests/generic_scheduler/model_test.py
@@ -35,6 +35,45 @@ class Test(unittest.TestCase):
         self.assertEqual(all_within_capacity(servers), True, 'all servers within capacity')
         self.assertEqual(all_allocated(servers, VMs), True, 'all VMs are allocated')
 
+    def test_state(self):
+        import copy
+        # some servers
+        s1 = Server(4000, 2)
+        s2 = Server(8000, 4)
+        servers = [s1, s2]
+        # some VMs
+        vm1 = VM(2000, 1);
+        vm2 = VM(1000, 1);
+        vm3 = VM(1000, 1);
+        VMs = [vm1,vm2,vm3]
+        s1.alloc.add(vm1)
+        s2.alloc.add(vm2)
+        s2.alloc.add(vm3)
+        a = State(servers, VMs)
+        b = copy.deepcopy(a)
+        b.servers[0].alloc.remove(vm1)
+        self.assertIn(vm1, a.servers[0].alloc, 'changing one state must not affect the other')
+
+    def test_migration(self):
+        # some servers
+        s1 = Server(4000, 2)
+        s2 = Server(8000, 4)
+        servers = [s1, s2]
+        # some VMs
+        vm1 = VM(2000, 1);
+        VMs = [vm1]
+        
+        # initial position
+        s1.alloc.add(vm1)
+        
+        a = State(servers, VMs)
+        migr = Migration(vm1, s2)
+        b = a.transition(migr)
+        self.assertIn(vm1, b.servers[1], 'vm1 should have moved after the transition')
+        #TODO: refactor - alloc should be in state!!!
+        print(b)
+        
+        #self.assertIn(vm1, b.servers[s2].alloc, 'VM should be on s2 in state b')
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
