de1c746ae1bb73c9d3d4cef1fc0ab193fa1bcad7
Dražen Lučanin <kermit666@gmail.com>
2014-03-03 16:01:49 +0100

    ScheduleUnit.mutation implementation

--- philharmonic/scheduler/ga/gascheduler.py
+++ philharmonic/scheduler/ga/gascheduler.py
@@ -1,4 +1,10 @@
-from philharmonic import Schedule
+import copy
+import random
+
+import pandas as pd
+import numpy as np
+
+from philharmonic import Schedule, Migration
 from philharmonic.scheduler.evaluator import normalised_combined_cost
 
 class ScheduleUnit(Schedule):
@@ -7,6 +13,29 @@ class ScheduleUnit(Schedule):
         #TODO: maybe move this method to the Scheduler
         el_prices = self.environment.current_data()
         normalised_cost = normalised_combined_cost(self.cloud, self.environment,
-                                                    self, el_prices)
+                                                   self, el_prices)
         # TODO: add constraint and SLA penalties
         return normalised_cost
+
+    def mutation(self):
+        # copy the ScheduleUnit
+        new_unit = copy.copy(self)
+        # remove one action
+        i = random.randint(0, len(self.actions)-1)
+        new_unit.actions = new_unit.actions.drop(self.actions.index[i])
+        # add new random action
+        # - pick random moment
+        # TODO: turn into function in timeseries (used in inputgen - DRY)
+        start = self.environment.t
+        end = self.environment.forecast_end
+        delta = end - start
+        offset = pd.offsets.Second(np.random.uniform(0., delta.total_seconds()))
+        t = start + offset
+        t = pd.Timestamp(t.date()) + pd.offsets.Hour(t.hour) # round to hour
+        # - pick random VM (among union of all allocs at t and VMRequests)
+        vm = random.sample(self.environment.VMs, 1)[0]
+        # - pick random server
+        server = random.sample(self.cloud.servers, 1)[0]
+        new_action = Migration(vm, server)
+        new_unit.add(new_action, t)
+        return new_unit
--- philharmonic/scheduler/ga/tests/test_gascheduler.py
+++ philharmonic/scheduler/ga/tests/test_gascheduler.py
@@ -41,3 +41,33 @@ def test_fitness():
     unit2.environment = env
     fitness2 = unit2.calculate_fitness()
     assert_true(fitness < fitness2, 'unit migrates to cheaper location faster')
+
+def test_mutation():
+    unit = ScheduleUnit()
+
+    # cloud
+    vm1 = VM(4,2)
+    vm2 = VM(4,2)
+    server1 = Server(8,4, location="A")
+    server2 = Server(8,4, location="B")
+    unit.cloud = Cloud([server1, server2])
+
+    # actions
+    t1 = pd.Timestamp('2013-02-25 00:00')
+    t2 = pd.Timestamp('2013-02-25 13:00')
+    times = [t1, t2]
+    actions = [Migration(vm1, server1), Migration(vm2, server2)]
+    unit.actions = pd.Series(actions, times)
+
+    # environment
+    times = pd.date_range('2013-02-25 00:00', periods=48, freq='H')
+    env = GASimpleSimulatedEnvironment(times)
+    env.t = t1
+    env.VMs = set([vm1, vm2]) # represents all VM requests
+    env.el_prices = inputgen.simple_el()
+    unit.environment = env
+
+    mutated = unit.mutation()
+    assert_is_instance(mutated, ScheduleUnit)
+    assert_true((unit.actions.values == actions).all(), 'original unchanged')
+    assert_true((mutated.actions != unit.actions).any(), 'mutated changed')
