f37625167fc44fcdd6073e1918d3db85e19866da
Dražen Lučanin <kermit666@gmail.com>
2014-12-16 12:28:13 +0000

    BCFFS includes the BCF functionality

--- philharmonic/scheduler/bcffs_scheduler.py
+++ philharmonic/scheduler/bcffs_scheduler.py
@@ -1,8 +1,9 @@
 from philharmonic.scheduler.ischeduler import IScheduler
+from philharmonic.scheduler.bcf_scheduler import *
 from philharmonic import Schedule, Migration
 from philharmonic import conf
 
-class BCFFSScheduler(IScheduler):
+class BCFFSScheduler(BCFScheduler):
     """Best Cost Fit Frequency Scaling (BCFFS) scheduling algorithm.
     In the first stage migrates VMs to servers maximising utilisation
     and favouring locations with lower electricity and cooling costs.
@@ -13,4 +14,23 @@ class BCFFSScheduler(IScheduler):
     """
     def reevaluate(self):
         self.schedule = Schedule()
+        self._original_vm_hosts = {} # stores original VM allocations
+        t = self.environment.get_time() # current time
+        # get VMs that need to be (re-)allocated
+        #  - VMs from boot requests
+        requests = self.environment.get_requests()
+        VMs = [req.vm for req in requests if req.what == 'boot']
+        #  - select VMs on underutilised PMs
+        VMs.extend(self._remove_vms_from_underutilised_hosts())
+        VMs = sort_vms_big_first(VMs)
+
+        # schedule migrations
+        for vm in VMs:
+            host = self.find_host(vm)
+            if host is None:
+                raise Exception("not enough free resources")
+            self._place(vm, host, t)
+
+        # schedule frequency scaling
+        #import ipdb; ipdb.set_trace()
         return self.schedule
---
+++ philharmonic/scheduler/tests/test_bcffs_scheduler.py
@@ -0,0 +1,46 @@
+from nose.tools import *
+from mock import MagicMock
+import pandas as pd
+
+from philharmonic import Schedule
+from philharmonic.scheduler.bcffs_scheduler import *
+
+from philharmonic.simulator.environment import FBFSimpleSimulatedEnvironment
+from philharmonic.simulator.simulator import FBFSimulator
+from philharmonic.simulator import inputgen
+from philharmonic import Cloud, VMRequest, \
+    VM, Server, State, Migration
+
+def test_bcffs_returns_schedule():
+    scheduler = BCFFSScheduler()
+    scheduler.cloud = Cloud()
+    scheduler.environment = FBFSimpleSimulatedEnvironment()
+    scheduler.environment.get_requests = MagicMock(return_value = [])
+    scheduler.environment.get_time = MagicMock(return_value = 1)
+    schedule = scheduler.reevaluate()
+    assert_is_instance(schedule, Schedule)
+
+def test_bcf_reevaluate_underutilised():
+    scheduler = BCFFSScheduler()
+    scheduler.environment = FBFSimpleSimulatedEnvironment()
+    s1, s2 = Server(40000, 12, location='A'), Server(20000, 10, location='A')
+    vm1 = VM(2000, 1)
+    vm2 = VM(2000, 1)
+    r2 = VMRequest(vm2, 'boot')
+    cloud = Cloud([s1, s2], [vm1, vm2])
+    scheduler.cloud = cloud
+    scheduler.environment.get_requests = MagicMock(return_value = [r2])
+    scheduler.environment.t = 1
+    el = pd.DataFrame({'A': [0.08], 'B': [0.08]}, [1])
+    temp = pd.DataFrame({'A': [15], 'B': [15]}, [1])
+    scheduler.environment.current_data = MagicMock(return_value = (el, temp))
+    # the initial state is a VM hosted on an underutilised PM
+    cloud.apply_real(Migration(vm1, s2))
+    schedule = scheduler.reevaluate()
+    for action in schedule.actions:
+        cloud.apply_real(action)
+    current = cloud.get_current()
+    assert_true(current.all_within_capacity())
+    assert_equals(current.allocation(vm1), s1)
+    assert_equals(current.allocation(vm2), s1)
+    assert_true(current.all_allocated())
