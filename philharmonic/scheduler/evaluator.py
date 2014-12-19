093892b5c9ddb4bdbf2499ff0c8425e35353dc22
Ilia Pietri <iliapietri@gmail.com>
2014-12-19 17:09:00 +0000

    relative RAM size

--- philharmonic/scheduler/evaluator.py
+++ philharmonic/scheduler/evaluator.py
@@ -650,7 +650,7 @@ def calculate_service_profit(cloud, environment, schedule,
     df_beta = pd.DataFrame([{vm : vm.beta for vm in cloud.get_current().vms}],
                            [start])
     df_beta = df_beta.reindex(freq.index, method='pad')
-    ram_size_base=1024
+    ram_size_base=1#1024
     ram_index='RAM'
     df_rel_ram = pd.DataFrame([{vm : vm.res[ram_index]/ram_size_base for vm in cloud.get_current().vms}],
                            [start])
