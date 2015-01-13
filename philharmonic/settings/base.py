572ac2162200e14eb2c6129de15eefc6485fe605
Dražen Lučanin <kermit666@gmail.com>
2015-01-13 12:00:07 +0100

    typo in the config

--- philharmonic/settings/base.py
+++ philharmonic/settings/base.py
@@ -45,7 +45,7 @@ DATA_LOC = os.path.realpath(os.path.join(os.path.dirname(__file__),
 
 # the datasets used in the simulation
 USA = False # USA or world-wide
-FIXED_TEMP = False # fixed el. prices world-wide
+FIXED_EL_PRICES = False # fixed el. prices world-wide
 DATA_LOC_USA = os.path.join(DATA_LOC, "usa/")
 DATA_LOC_WORLD = os.path.join(DATA_LOC, "world/")
 DATA_LOC_WORLD_FIXED_EL = os.path.join(DATA_LOC, "world-fixed_el/")
@@ -53,7 +53,7 @@ if USA:
     DATA_LOC = DATA_LOC_USA
 else:
     DATA_LOC = DATA_LOC_WORLD
-    if FIXED_TEMP:
+    if FIXED_EL_PRICES:
         DATA_LOC = DATA_LOC_WORLD_FIXED_EL
 temperature_dataset = os.path.join(DATA_LOC, 'temperatures.csv')
 el_price_dataset = os.path.join(DATA_LOC, 'prices.csv')
