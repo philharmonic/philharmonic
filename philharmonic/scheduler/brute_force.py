82781e54233016281460f36161276dc4e91ea641
Dražen Lučanin <kermit666@gmail.com>
2015-04-11 11:43:25 +0200

    fix missing import

--- philharmonic/scheduler/brute_force.py
+++ philharmonic/scheduler/brute_force.py
@@ -1,8 +1,9 @@
 from philharmonic.scheduler.ischeduler import IScheduler
+from philharmonic import Schedule
 
 class BruteForceScheduler(IScheduler):
     """Deterministic brute force scheduler."""
-    
+
     def reevaluate(self):
         """Look at the current state of the Cloud and Environment
         and schedule new/different actions if necessary.
